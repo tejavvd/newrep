5cab0e23ce26395b267ad6d68b639bd1fa85c422
surya.asa@gmail.com <surya.asa@gmail.com>
2017-09-23 01:24:51 +0530

    Commie

--- src/main/java/com/sample/simple/App.java
+++ src/main/java/com/sample/simple/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Whatishyt upppp! Addy New afafafa" );
+        System.out.println( "Check 1" );
     }
 }
