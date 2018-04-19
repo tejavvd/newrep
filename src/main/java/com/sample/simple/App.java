5b2ad691cfe311506086b5f1c9c757a5998412a5
surya.asa@gmail.com <surya.asa@gmail.com>
2018-04-19 12:29:10 +0530

    Third Commit

--- src/main/java/com/sample/simple/App.java
+++ src/main/java/com/sample/simple/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is AWS Cloud yo" );
+        System.out.println( "This is AWS Cloud yooyrorj" );
     }
 }
